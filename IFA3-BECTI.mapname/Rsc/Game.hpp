ad7a9e27f5f1308bf5dfe6395cca20ee65070d58
dukee02 <68999242+dukee02@users.noreply.github.com>
2022-03-31 14:05:43 +0200

    Changed: Spawn logic for town units
    Specific spawn locations can be placed in the editor
    Spawn with less collisions, only when when to much vehicles gets placed (needs other fix)

--- IFA3-BECTI.mapname/Rsc/Game.hpp
+++ IFA3-BECTI.mapname/Rsc/Game.hpp
@@ -6,9 +6,9 @@ respawnDialog = false;
 //--- Require briefing.html to show up.
 onLoadMission = "VIO-BECTI-IFA3 V2.0";
 onLoadMissionTime = false;
-onLoadName = "VIO-BECTI-IFA3 V2.0_220214";
+onLoadName = "VIO-BECTI-IFA3 V2.0_220331";
 
-briefingName = "VIO-BECTI-IFA3 V2.0_220214";
+briefingName = "VIO-BECTI-IFA3 V2.0_220331";
 
 //--- ArmA 3 Specifics
 dev = "Benny, dukee";
