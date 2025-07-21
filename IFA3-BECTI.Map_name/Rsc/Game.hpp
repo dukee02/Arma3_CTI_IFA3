fb4c4776358b4c26d1ae55ef241729a1d8929ee2
dukee02 <68999242+dukee02@users.noreply.github.com>
2025-07-21 13:55:03 +0200

    Changed: default - Inf Marker now show icon for own units

--- IFA3-BECTI.Map_name/Rsc/Game.hpp
+++ IFA3-BECTI.Map_name/Rsc/Game.hpp
@@ -8,7 +8,7 @@ onLoadName = "VIO-BECTI-SPE_IFA3 V2.0.6beta";
 onLoadMissionTime = false;
 overviewText = "VIO-BECTI-SPE_IFA3 V2.0.6beta";
 overviewTextLocked = "VIO-BECTI-SPE_IFA3 V2.0.6beta";
-onLoadMission = "VIO-BECTI-SPE_IFA3 V2.0.6beta_250718";
+onLoadMission = "VIO-BECTI-SPE_IFA3 V2.0.6beta_250720";
 briefingName = "VIO-BECTI-SPE_IFA3 V2.0.6beta";
 
 //--- ArmA 3 Specifics
